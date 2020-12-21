5e8dd9e2e92db46d2e50dffa8eea5918b891a71e
王欢 <wanghuan@wanghuandeMacBook-Air.local>
2020-12-21 22:18:05 +0800

    init

---
+++ src/main/java/com/uwjx/mongo/MongoDbTestingApplication.java
@@ -0,0 +1,13 @@
+package com.uwjx.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDbTestingApplication.class, args);
+    }
+
+}
