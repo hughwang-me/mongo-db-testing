3ed0b149424e9942b4b87bfea1cd80c4733feea8
Hugh Wang <18501667737@163.com>
2020-12-23 18:51:09 +0800

    quick push by bash.

--- src/main/java/com/uwjx/mongo/service/MongoDBTestService.java
+++ src/main/java/com/uwjx/mongo/service/MongoDBTestService.java
@@ -4,9 +4,16 @@ import com.alibaba.fastjson.JSON;
 import com.uwjx.mongo.entity.AccountEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 
@@ -22,17 +29,20 @@ public class MongoDBTestService {
         String dbName = mongoTemplate.getDb().getName();
         log.warn("dbName : {}" , dbName);
 
-        AccountEntity accountEntity = new AccountEntity();
-        accountEntity.setId("1001");
-        accountEntity.setName("wanghuan");
-        accountEntity.setPassword("9999991");
+        for (int i = 0; i < 10; i++) {
+            AccountEntity accountEntity = new AccountEntity();
+            accountEntity.setId(String.valueOf(1000 + i));
+            accountEntity.setName("wanghuan" + i);
+            accountEntity.setPassword("9999991");
+            mongoTemplate.save(accountEntity);
+        }
 
-        mongoTemplate.save(accountEntity);
 
-        AccountEntity accountEntityQueryResult = mongoTemplate.findOne(new Query(where("name").is("wanghuan")) , AccountEntity.class);
-        log.warn("query result -> {}" , JSON.toJSONString(accountEntityQueryResult) );
 
-        mongoTemplate.remove(new Query(where("name").is("wanghuan")) , AccountEntity.class);
+//        AccountEntity accountEntityQueryResult = mongoTemplate.findOne(new Query(where("name").is("wanghuan")) , AccountEntity.class);
+//        log.warn("query result -> {}" , JSON.toJSONString(accountEntityQueryResult) );
+//
+//        mongoTemplate.remove(new Query(where("name").is("wanghuan")) , AccountEntity.class);
     }
 
     public void delete(){
@@ -44,7 +54,38 @@ public class MongoDBTestService {
     }
 
     public void list(){
-        AccountEntity accountEntityQueryResult = mongoTemplate.findOne(new Query(where("name").is("wanghuan2")) , AccountEntity.class);
-        log.warn("query result -> {}" , JSON.toJSONString(accountEntityQueryResult) );
+        Query query = new Query();
+//        query.addCriteria(Criteria.where("g"))
+//        query.skip(3);
+//        query.limit(4);
+//        query.with(Sort.by(
+//           Sort.Order.asc("_id")
+//        ));
+        Sort sort = Sort.by(
+          Sort.Order.asc("_id")
+        );
+        Pageable pageable = PageRequest.of(0,3 , sort);
+        query.with( pageable);
+        List<Map> accountEntityQueryResult = mongoTemplate.find(query , Map.class , "salary");
+        log.warn("query result -> {}" ,accountEntityQueryResult.size());
+
+        for (Map accountEntity : accountEntityQueryResult) {
+            log.warn("id -> {}   工号 -> {}" , accountEntity.get("_id") , accountEntity.get("工号"));
+        }
+    }
+
+    public void list2(){
+        Query query = new Query();
+        query.skip(3);
+        query.limit(4);
+        query.with(Sort.by(
+                Sort.Order.asc("id")
+        ));
+        List<AccountEntity> accountEntityQueryResult = mongoTemplate.find(query , AccountEntity.class , "accountEntity");
+        log.warn("query result -> {}" ,accountEntityQueryResult.size());
+
+        for (AccountEntity accountEntity : accountEntityQueryResult) {
+            log.warn("id -> {}" , accountEntity.getName());
+        }
     }
 }
